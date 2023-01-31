759188d4980b327d5dce810d72bc85debbf6345a
Greg Griffith <griffith_g@hotmail.com>
2023-01-30 19:03:20 -0500

    seeded books and reviews table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,11 @@
-check
+INSERT INTO books (book_title, book_author, book_description)
+VALUES ("The Color Purple","Alice Walker","Only a handful of books have managed to win both the Pulitzer Prize and a National Book Award—that should give you a sense of how much this book has resonated with readers. "),
+       ("The Adventures of Huckleberry Finn","Mark Twain","Mark Twain is known as the father of American literature, and anyone who's read his work can see why. Twain's prose is simple yet mesmerizing, and his storytelling abilities are still unparalleled more than a century later."),
+       ("One Hundred Years of Solitude","Gabriel Garcia Marquez","This novel was written by a winner of the Nobel Prize, is one of the most famous examples of magical realism, and has been listed by Joan Didion as one of the books that changed her life. "),
+       ("Don Quixote","Miguel de Cervantes","If you don't know what it means to tilt at windmills, then you owe it to yourself to cross Don Quixote off your reading bucket list. This 17th century treasure follows a nobleman who has lost his mind and proceeds to have insane adventures in the name of chivalry. "),
+       ("Charlotte's Web","E.B. White","If you somehow made it through childhood without reading Charlotte's Web—or if you did, but no longer remember it—stop what you're doing and pick up a copy of this book. A regular essayist for the New Yorker and the co-author of The Elements of Style, a guide that will likely never leave syllabi for college writing courses, no one writes like E.B. White. ");
+
+INSERT INTO reviews (book_id, review)
+VALUES ("1","Interesting read"),
+       ("1","Very captivating"),
+       ("1","What a great book");
