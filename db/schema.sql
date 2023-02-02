c9ecac5def0b85ab86ef9af93504bc4d97a25c1a
Greg Griffith <griffith_g@hotmail.com>
2023-02-01 19:32:04 -0500

    updated home routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,5 @@
 DROP DATABASE IF EXISTS bookclub_db;
 CREATE DATABASE bookclub_db;
 
-USE bookclub_db;
-
-CREATE TABLE books (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    book_title VARCHAR(50),
-    book_author VARCHAR(50),
-    book_description TEXT
-);
-
-CREATE TABLE reviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    book_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (book_id)
-    REFERENCES books(id)
-    ON DELETE SET NULL
-)
 
 
